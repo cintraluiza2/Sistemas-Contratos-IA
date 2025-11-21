5cc0d68ca833c3f888a60aa37f5148c4565bc8de
Mateus Ribeiro <mateusribeirus@gmail.com>
2025-11-20 23:25:30 -0300

    Update .env

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,11 @@ from docxtpl import DocxTemplate
 import warnings
 warnings.filterwarnings("ignore", message="pkg_resources is deprecated")
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="/var/www/Sistemas-Contratos-IA/.env")
+
 # ---------- Configuração ----------
 app = Flask(__name__)
 CORS(app)
